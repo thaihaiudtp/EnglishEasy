64f2e528a0248bb51548c5cbbe906b74a74c5720
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-01-09 18:17:55 +0700

    update

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://ctb02thaiha:09022004@cluster0.tgl4y.mongodb.net/EnglishEasy?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('mongodb+srv://YtYZdePY:04PiKjNKlDio3JiN@us-east-1.ufsuw.mongodb.net/test-52Jo')
         console.log("ok!")
     } catch (error) {
         console.log(error);
