f61abe01375b49de22c55d483ad5bf26f50035e2
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-01-25 17:31:54 +0700

    ok1

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/CRUD_LEARN'/*'mongodb+srv://YtYZdePY:04PiKjNKlDio3JiN@us-east-1.ufsuw.mongodb.net/test-52Jo'*/)
+        await mongoose.connect('mongodb+srv://YtYZdePY:04PiKjNKlDio3JiN@us-east-1.ufsuw.mongodb.net/test-52Jo'/*'mongodb+srv://YtYZdePY:04PiKjNKlDio3JiN@us-east-1.ufsuw.mongodb.net/test-52Jo'*/)
         console.log("ok!")
     } catch (error) {
         console.log(error);
